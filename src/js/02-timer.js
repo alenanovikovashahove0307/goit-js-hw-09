38e2e5fe2a9b0bcf03188e3a0585eac308073bb4
alenanovikovashahove0307 <novikova-fin@ukr.net>
2023-05-25 21:43:02 +0300

    Виконано завдання 2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,73 +1,93 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 
+let timerID = null;
+
 const refs = {
-    inputEl: document.getElementById('datetime - picker'),
-    startBtn: document.querySelector('[data-start]'),
-    dayEl: document.querySelector('[data-days]'),
-    hoursEl: document.querySelector('[data-hours]'),
-    minutesEl: document.querySelector('[data-minutes]'),
-    secondsEl: document.querySelector('[data-seconds]')
+  inputDate: document.getElementById('datetime-picker'),
+  btnStart: document.querySelector('button[data-start]'),
+
+  dataDaysEl: document.querySelector('span[data-days]'),
+  dataHoursEl: document.querySelector('span[data-hours]'),
+  dataMinutesEl: document.querySelector('span[data-minutes]'),
+  dataSecondsEl: document.querySelector('span[data-seconds]'),
+};
+
+const optionsFlatpickr = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    const currentDate = new Date().getTime();
+    const timerInSeconds = selectedDates[0] - currentDate;
+
+    if (timerInSeconds <= 0) {
+      
+      Notify.failure('Please choose a date in the future');
+      return null;
+    }
+
+    const objTimer = convertMs(timerInSeconds);
+
+    renderTimer(objTimer);
+
+    refs.btnStart.disabled = false;
+    refs.inputDate.disabled = true;
+  },
+};
+
+refs.btnStart.disabled = true;
+refs.btnStart.addEventListener('click', onClickStart);
+
+const objFlatpickr = flatpickr(refs.inputDate, optionsFlatpickr);
+
+
+function onClickStart() {
+  refs.btnStart.disabled = true;
+
+  const selectTime = objFlatpickr.latestSelectedDateObj.getTime();
+  
+  timerID = setInterval(startTime, 1000, selectTime);
 }
-refs.startBtn.setAttribute(`disabled`, true);
-const currentDate = new Date();
+
+function startTime(selectTime) {
+  let currentDate = selectTime - new Date().getTime();
+
+  if (currentDate <= 0) {
+    
+    clearInterval(timerID);
+    renderTimer({ days: '00', hours: '00', minutes: '00', seconds: '00' });
+    refs.btnStart.disabled = true;
+    refs.inputDate.disabled = false;
+    return;
+  }
+
+  const objTimer = convertMs(currentDate);
+  renderTimer(objTimer);
+}
+
+
+function renderTimer(objTimer) {
+  refs.dataDaysEl.innerText = String(objTimer.days).padStart(2, 0);
+  refs.dataHoursEl.innerText = String(objTimer.hours).padStart(2, 0);
+  refs.dataMinutesEl.innerText = String(objTimer.minutes).padStart(2, 0);
+  refs.dataSecondsEl.innerText = String(objTimer.seconds).padStart(2, 0);
+}
 
 function convertMs(ms) {
+  
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
+
+  
   const days = Math.floor(ms / day);
   const hours = Math.floor((ms % day) / hour);
   const minutes = Math.floor(((ms % day) % hour) / minute);
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
   return { days, hours, minutes, seconds };
 }
-  function faddLeadingZero(value) {
-  return String(value).padStart(2, '0');
-}
-function onScreenLoad(object) {
-    days.textContent = padStart(object.days);
-    hours.textContent = padStart(object.hours);
-    minutes.textContent = padStart(object.minutes);
-    seconds.textContent = padStart(object.seconds);
-};
-  function counter(firstDate){ 
-    // const currentDate = new Date();
-    const countdownTime = convertTime(firstDate.getTime() - currentDate.getTime());
-    console.log(countdownTime);
-    onScreenLoad(countdownTime);
-    if ((countdownTime.days === 0 && countdownTime.hours === 0 && countdownTime.minutes === 0 && countdownTime.seconds ===0)){
-        clearInterval(timerInterval);
-        days.textContent = faddLeadingZero(0);
-        hours.textContent = faddLeadingZero(0);
-        minutes.textContent = faddLeadingZero(0);
-        seconds.textContent = faddLeadingZero(0);
-    }
-}
-
-
-const options = {
-  enableTime: true,
-  time_24hr: true,
-  defaultDate: new Date(),
-  minuteIncrement: 1,
-    onClose(selectedDates) {
-      const firstDate = selectedDates[0];
-    if (firstDate < currentDate) {
-      window.alert('Please choose a date in the future');
-    } else {
-      startBtn.removeAttribute(`disabled`, true);
-      console.log(firstDate.getTime());
-      console.log(currentDate.getTime());
-      startBtn.addEventListener('click', startCounter);
-      
-      function startCounter() {
-        clearInterval(timerInterval);
-        timerInterval = setInterval(() => counter(firstDate), 1000);
-        startBtn.setAttribute(`disabled`, true);
-      }
-    }
-  },
-};
-flatpickr('#datetime-picker', 'options');
